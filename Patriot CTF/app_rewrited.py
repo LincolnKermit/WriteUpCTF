12aaccc87b7ac954c8bb85d3dd1b957e8f03a263
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2024-09-22 16:04:05 +0200

    Update app_rewrited.py

--- Patriot CTF/app_rewrited.py
+++ Patriot CTF/app_rewrited.py
@@ -3,6 +3,8 @@ import uuid
 import os
 from datetime import datetime, timedelta
 import hashlib
+
+#  
 app = Flask(__name__)
 server_start_time = datetime(2024, 9, 21, 10, 39, 15)
 server_start_str = server_start_time.strftime('%Y%m%d%H%M%S')
@@ -10,15 +12,11 @@ secure_key = "5366f780393b7cdddfc817fab3cbfbbbb6078c3952733a569d586c0089b2a1f0"
 app.secret_key = secure_key
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=300)
 
-# 5366f780393b7cdddfc817fab3cbfbbbb6078c3952733a569d586c0089b2a1f0
 
-
-
-from flask import Flask, session
+# Import flask cookie encoder
 from itsdangerous import URLSafeTimedSerializer
-import uuid, hashlib
 secret = uuid.UUID('31333337-1337-1337-1337-133713371337')
-
+# Take the admin id and translate it into UUID
 
 
 
@@ -27,6 +25,7 @@ app = Flask(__name__)
 
 # Set the known secret key (this should be the same as the one used by the Flask app)
 app.secret_key = '5366f780393b7cdddfc817fab3cbfbbbb6078c3952733a569d586c0089b2a1f0'
+# Take the secret from the date and hour of the server based on the start time
 
 # Create a serializer for generating a cookie
 def create_signed_cookie(data, secret_key):
@@ -50,8 +49,4 @@ with app.app_context():
     fake_cookie = create_signed_cookie(fake_session_data, app.secret_key)
     print("Fake cookie:", fake_cookie)
 
-
-
-
-
-
+# Create cookie and check it with https://www.kirsle.net/wizards/flask-session.cgi
